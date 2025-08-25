ad7976d90a7905d9b34072a7e50636fdd2b3a2b8
Aleksei Sidorenko <Sidorenko1202@yandex.ru>
2025-08-25 20:46:38 +0300

    ver_5

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
+# isort: off
 from api import views
+# isort: on
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
